4006f07375028a1fe8e9cf30f68f9985f0853de1
황승찬 <seungchan@hwangseungchan-ui-MacBookPro.local>
2024-06-24 13:54:13 +0900

    chapter 2 clear

--- ch2/helloSLAM.cpp
+++ ch2/helloSLAM.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
-using namespace std;
+#include <iostream>   //standard IO stream library (cout, cin, cerr 등의 객체 정의)
+using namespace std;  //using standard namespace(std) - std::cout 대신 cout 사용 가능
 
-int main(int argc, char **argv) {
-  cout << "Hello SLAM!" << endl;
+int main(int argc, char **argv) {   //argc: 명령줄 인자 개수  argv: 명령줄 인자의 문자열 포인터 배열 (argv[0] = 실제 프로그램, argv[1] 부터 명령어)
+  cout << "Hello SLAM!" << endl; 
+  //cout: stream insert (출력할 내용을 cout stream에 insert)
+  //endl: newline 출력 + 출력 stream flush
   return 0;
 }
--- ch2/libHelloSLAM.h
+++ ch2/libHelloSLAM.h
@@ -1,8 +1,15 @@
+// library 파일(.a, .so)만 있으면 어떤 함수가 있는지, 호출의 형식이 뭔지 알 수 없다
+// library file은 컴파일된 이진 함수가 포함된 압축 패키지일뿐
+// 누군가가 library를 사용하기 위해 라이브러리에 무엇이 있는지 설명하는 header 파일이 필요하다
+// 라이브러리 사용자는 header와 라이브러리 파일만 있으면 호출 가능
+
 #ifndef LIBHELLOSLAM_H_
 #define LIBHELLOSLAM_H_
 // 上面的宏定义是为了防止重复引用这个头文件而引起的重定义错误
+// 위의 매크로 정의는 이 헤더 파일의 중복 인용으로 인한 재정의 오류를 방지하기 위한 것
 
 // 打印一句hello的函数
+// Hello 함수 출력
 void printHello();
 
 #endif
